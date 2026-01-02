20e4d652baf6cf756a1af9cc994afb0bd708a6b1
Steven Budworth <budworts@googlemail.com>
2026-01-02 12:23:09 +0000

    Original

--- strings2.py
+++ strings2.py
@@ -39,3 +39,19 @@ print(parrot[:9])
 print(parrot[10:14])
 print(parrot[10:])
 print(parrot[:])
+print(parrot[-4:-2]) # Bl
+print(parrot[-4:12]) # Bl
+
+print(parrot[0:6:2])
+print(parrot[0:6:3])
+
+print()
+number = "9,223,372,036,854,775,807"
+print(number[1::4])
+number = "9,223;372:036 854,775;807"
+print(number[1::4])
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
